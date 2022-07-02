349043ebb9236b715dcfc652a556801460f4f64a
YamCast <100479274+YamCast@users.noreply.github.com>
2022-07-02 02:03:21 -0500

    Add files via upload

---
+++ yamcast/boton_script.js
@@ -0,0 +1,97 @@
+tippy('#myBtn', '#mylogo', {
+  content: "invita",
+  placement: 'right',
+  animation: 'fade',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+
+tippy('#myBtn', {
+  content: "Inicio",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+
+tippy('#mylogo', {
+  content: "Invita",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+tippy('#server1', {
+  content: "Servidor 1",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+tippy('#server2', {
+  content: "Servidor 2",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+tippy('#server3', {
+  content: "Servidor 3",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+tippy('#server4', {
+  content: "Servidor 4",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+tippy('#anadirServidor', {
+  content: "Añadir un servidor",
+  placement: 'right',
+  animation: 'perspective',
+  theme: 'tomato',
+});
+
+tippy('#explora', {
+  content: "Explora servidores públicos",
+  placement: 'right',
+  animation: 'fade',
+  theme: 'tomato',
+});
+
+
+tippy('descarg', {
+  content: "Descarga Aplicaciones",
+  placement: 'right',
+  animation: 'fade',
+  theme: 'tomato',
+});
+
+
+tippy('micro', {
+  content: "Activar sondo",
+  animation: 'fade',
+  theme: 'tomato',
+});
+tippy('auricu', {
+  content: "Ensordecer",
+  animation: 'fade',
+  theme: 'tomato',
+});
+tippy('ajustes', {
+  content: "Ajustes de Usuario",
+  animation: 'fade',
+  theme: 'tomato',
+});
+
+
+tippy('#bot', {
+  content: "Bot Verificado",
+  animation: 'fade',
+  theme: 'tomato',
+});
